1b07b96cbb781166212d3bfa89b46d2673e1169d
jaseemkm <napsterjaseem@gmail.com>
2018-07-02 00:05:36 +0530

    addedModelsForVideo

--- youtubeJukebox/models.py
+++ youtubeJukebox/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+	url = models.URLField()
+	videoId = models.CharField(max_length=100,unique=True)
+	vote = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.videoId
