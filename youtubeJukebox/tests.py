d1c20177cafcf8d0b6b46aea57175946188772c1
Nithin Mohan <nithinmohan114@gmail.com>
2019-10-10 16:33:42 +0530

    Add test on video add item

--- youtubeJukebox/tests.py
+++ youtubeJukebox/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+import pytest
+from management.commands import listener
+from youtubeJukebox.models import Video
 
-# Create your tests here.
+
+@pytest.mark.django_db
+def test_adding_video_item():
+    link = "https://www.youtube.com/BBAyRBTfsOU"
+    added_item = listener.add_item(link)
+    expected_result = Video.objects.filter(videoId="BBAyRBTfsOU")
+    assert added_item == expected_result
