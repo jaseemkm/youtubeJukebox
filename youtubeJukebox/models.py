9215ac5f0ffc015b65b6a99f59c67857bc3f58fd
Nithin Mohan <nithinmohan114@gmail.com>
2019-10-11 15:30:05 +0530

    Update models

--- youtubeJukebox/models.py
+++ youtubeJukebox/models.py
@@ -1,20 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Video(models.Model):
     url = models.URLField()
-    videoId = models.CharField(max_length=100,unique=True)
+    videoId = models.CharField(max_length=100, unique=True)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.videoId
 
+
 class User(models.Model):
-    user = models.CharField(max_length=100,unique=True)
-    user_id = models.CharField(max_length=100,unique=True)
+    user = models.CharField(max_length=100, unique=True)
+    user_id = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.user_id
+
 
 class Vote(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
