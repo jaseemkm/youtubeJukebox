f5409c29675e0d401b34e4225433783573f6a790
jaseemkm <napsterjaseem@gmail.com>
2020-10-31 13:50:06 +0530

    Update Video Django Admin

--- youtubeJukebox/admin.py
+++ youtubeJukebox/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
 from .models import User, Video, Vote
+
+
+class VideoAdmin(admin.ModelAdmin):
+    readonly_fields = ['votes']
+
 #Register your models here.
 admin.site.register(User)
-admin.site.register(Video)
+admin.site.register(Video, VideoAdmin)
 admin.site.register(Vote)
